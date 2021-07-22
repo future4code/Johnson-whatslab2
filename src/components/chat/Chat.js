e77df6610a526992fcb061127159a4798f116606
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-07-21 23:51:16 -0300

    Implementado componentes de pesquisa de conversas e adicionar novas

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./chat.css";
-import ConteudoChat from './../conteudoChat/ConteudoChat';
-import Perfil from './../perfil/Perfil';
+import ConteudoChat from "./../conteudoChat/ConteudoChat";
+import Perfil from "./../perfil/Perfil";
+import Lista from "./../listaChat/ListaChat";
 
 export default class Chat extends React.Component {
   render() {
-    return <div className="chat">
+    return (
+      <div className="chat">
+        <Lista />
         <ConteudoChat />
         <Perfil />
-
-    </div>;
+      </div>
+    );
   }
 }
--- src/components/listaChat/ListaChat.js
+++ src/components/listaChat/ListaChat.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./listaChat.css";
 
 class ListaChat extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="lista">
+        <button className="botao">
+          <i className="fa fa-plus-square"></i>
+          <span>Nova Conversa</span>
+        </button>
+        <div className="lista-cabecalho">
+          <h2>Conversas</h2>
+          <button className="botao2">
+            <i className="fa fa-ellipsis-h"></i>
+          </button>
+        </div>
+        <div className="lista-pesquisar">
+          <div className="pesquisar">
+              <input type="text" placeholder="Pesquise aqui" required />
+              <button className="botao-pesquisar">
+                  <i className="fa fa-search"></i>
+              </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ListaChat;
