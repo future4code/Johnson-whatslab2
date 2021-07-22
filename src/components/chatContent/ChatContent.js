5748055a3b080819338045bc690f1ff8731078ef
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-07-21 23:13:23 -0300

    Implementado estrutura do projeto

--- src/components/chatContent/ChatContent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class ChatContent extends React.Component {
-    render() {
-        return <div>
-            
-        </div>
-    }
-}
--- src/components/userProfile/UserProfile.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class UserProfile extends React.Component {
-    render() {
-        return <div></div>
-    }
-}
