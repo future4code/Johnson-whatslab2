d2e71ca08c4708f5134f7f05578b1594d2c26800
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-07-21 23:05:12 -0300

    Finalizado icone na barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Nav from './components/nav/Nav.js';
 import Chat from "./components/chat/Chat";
 
 function App() {
   return (
     <div className="chat-main">
+      <Nav></Nav>
       <Chat />
     </div>
   );
