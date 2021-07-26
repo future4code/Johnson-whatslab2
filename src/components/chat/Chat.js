8e3637038252e2e7a621a87e329dac75fde7e67a
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-07-25 21:49:33 -0300

    Correção na seleção do usuario de envio da mensagem

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -5,11 +5,23 @@ import Perfil from "./../perfil/Perfil";
 import Lista from "./../listaChat/ListaChat";
 
 export default class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usuarioSelecionado: null,
+    };
+  }
+
+  perfilEscolhido = (usuario) => {
+    this.setState({ usuarioSelecionado: usuario });
+    console.log(usuario);
+  };
+
   render() {
     return (
       <div className="chat">
-        <Lista />
-        <ConteudoChat />
+        <Lista perfilEscolhido={this.perfilEscolhido} />
+        <ConteudoChat usuarioSelecionado={this.state.usuarioSelecionado} />
         <Perfil />
       </div>
     );
